a1e6919b2008f24c2704773b01fe553c0dc2a659
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-09-08 20:08:39 +0500

    fix: wire real routes into the entrypoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,37 +1,16 @@
 package main
 
 import (
-	"context"
+	"log"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 
-	"github.com/rs/zerolog/log"
+	"migrated-app/internal"
 )
 
 func main() {
-	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
-	defer stop()
-
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: buildRouter(),
-	}
-
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatal().Err(err).Msg("server error")
-		}
-	}()
-
-	log.Info().Msg("server started on :8080")
-	<-ctx.Done()
-
-	shutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(shutCtx); err != nil {
-		log.Error().Err(err).Msg("graceful shutdown failed")
+	mux := internal.BuildRouter()
+	log.Println("listening on :8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		log.Fatal(err)
 	}
 }
