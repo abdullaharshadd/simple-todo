006a4fb04ab2bf514e4e047b4e0287ff8383ed3a
Abdullah Arshad <57131829+abdullaharshadd@users.noreply.github.com>
2026-09-08 17:26:21 +0500

    chore: add go project scaffolding

---
+++ cmd/server/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
+func main() {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: buildRouter(),
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatal().Err(err).Msg("server error")
+		}
+	}()
+
+	log.Info().Msg("server started on :8080")
+	<-ctx.Done()
+
+	shutCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutCtx); err != nil {
+		log.Error().Err(err).Msg("graceful shutdown failed")
+	}
+}
